c92b52080c2f13fa4868472050f3fce9198411d0
Mustafa Derin <mustafa.derin@sabanciuniv.edu>
2025-11-16 23:29:11 +0300

    Add files via upload

---
+++ lib/data/recipes.dart
@@ -0,0 +1,45 @@
+import 'package:healthtrack_cs310project/recipe.dart';
+
+const List<Recipe> recipes = [
+  Recipe(
+    name: 'Spaghetti',
+    imageURL:
+    'https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/small',
+    calories: 250,
+    cookingTime: 15,
+  ),
+  Recipe(
+      name: 'Spaghetti',
+      imageURL:
+      'https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/small',
+      calories: 250,
+      cookingTime: 15,
+      isFavourite: true
+  ),
+  Recipe(
+      name: 'Spaghetti',
+      imageURL:
+      'https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/small',
+      calories: 250,
+      cookingTime: 15,
+      isVegetarian: true
+  ),
+  Recipe(
+      name: 'Spaghetti',
+      imageURL:
+      'https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/small',
+      calories: 250,
+      cookingTime: 15,
+      isLactoseFree: true
+  ),
+  Recipe(
+      name: 'Spaghetti',
+      imageURL:
+      'https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg/small',
+      calories: 250,
+      cookingTime: 15,
+      isVegetarian: true,
+      isLactoseFree: true,
+      isFavourite: true
+  ),
+];
