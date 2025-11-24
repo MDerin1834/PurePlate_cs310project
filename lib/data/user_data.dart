5cfab83bd6c8147a4994ce0d231e94785c8dbbb9
cerensenol <ceren.senol@sabanciuniv.edu>
2025-11-24 16:28:02 +0300

    added data for user 'user_data.dart'

---
+++ lib/data/user_data.dart
@@ -0,0 +1,15 @@
+import 'package:pure_plate/models/user.dart';
+
+User currentUser = User(
+  name: 'Ceren Şenol',
+  email: 'ceren.senol@example.com',
+  age: 21,
+  dietType: 'Balanced',
+  calorieTarget: 2000,
+  proteinTarget: 80,
+  profileImageUrl: null, // Will use local asset
+  isVegetarian: true,
+  isLactoseFree: false,
+  isGlutenFree: false,
+  isLowCarb: false,
+);
