aa8d43c6ce139d325a461d95caf698116d70f0ae
cerensenol <ceren.senol@sabanciuniv.edu>
2025-12-20 20:13:21 +0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:pure_plate/screens/onboarding_screen.dart';
 import 'package:pure_plate/screens/login_screen.dart';
 import 'package:pure_plate/screens/register_screen.dart';
@@ -13,24 +15,40 @@ import 'package:pure_plate/screens/edit_profile_screen.dart';
 import 'package:pure_plate/screens/records_screen.dart';
 import 'package:pure_plate/theme.dart';
 
-void main() {
-  runApp(MaterialApp(
-    theme: purePlateTheme,
-    initialRoute: '/onboarding',
-    routes: {
-      '/onboarding': (context) => const OnboardingScreen(),
-      '/login': (context) => const LoginScreen(),
-      '/register': (context) => const RegisterScreen(),
-      '/reset': (context) => const ResetPasswordScreen(),
-      '/home': (context) => const HomeScreen(),
-      '/': (context) => const HomeScreen(), // Default route
-      '/recipes': (context) => const RecipesScreen(),
-      '/filter': (context) => const RecipeFilteringScreen(),
-      '/filtered': (context) => const FilteredRecipesScreen(),
-      '/recipe-details': (context) => const RecipeDetailsScreen(),
-      '/profile': (context) => const ProfileScreen(),
-      '/edit-profile': (context) => const EditProfileScreen(),
-      '/records': (context) => const RecordsScreen(),
-    },
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase'i başlat
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: purePlateTheme,
+      initialRoute: '/onboarding',
+      routes: {
+        '/onboarding': (context) => const OnboardingScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/reset': (context) => const ResetPasswordScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/': (context) => const HomeScreen(), // Default route
+        '/recipes': (context) => const RecipesScreen(),
+        '/filter': (context) => const RecipeFilteringScreen(),
+        '/filtered': (context) => const FilteredRecipesScreen(),
+        '/recipe-details': (context) => const RecipeDetailsScreen(),
+        '/profile': (context) => const ProfileScreen(),
+        '/edit-profile': (context) => const EditProfileScreen(),
+        '/records': (context) => const RecordsScreen(),
+      },
+    );
+  }
 }
