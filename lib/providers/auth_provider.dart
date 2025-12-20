d7df968a0a1d8f3c7144869df08f9661943ac106
Mustafa Derin <mustafa.derin@sabanciuniv.edu>
2025-12-20 19:28:49 +0300

    Add files via upload

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,58 @@
+class AuthProvider extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? user;
+  bool isLoading = false;
+  String? errorMessage;
+
+  AuthProvider() {
+    _auth.authStateChanges().listen((u) {
+      user = u;
+      notifyListeners();
+    });
+  }
+
+  Future<void> login(String email, String password) async {
+    try {
+      isLoading = true;
+      errorMessage = null;
+      notifyListeners();
+
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      errorMessage = e.message;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> register(String email, String password) async {
+    try {
+      isLoading = true;
+      errorMessage = null;
+      notifyListeners();
+
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      errorMessage = e.message;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> resetPassword(String email) async {
+    await _auth.sendPasswordResetEmail(email: email);
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
