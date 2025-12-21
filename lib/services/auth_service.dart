25280a28c37df9d752268179ace11fab17fb652e
simay <simay@simay-MacBook-Pro-2.local>
2025-12-21 19:57:04 +0300

    Current local version with Firebase

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> login(String email, String password) async {
+    try {
+      final result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } on FirebaseAuthException catch (e) {
+      // HATAYI FIRLAT → Provider yakalayacak
+      throw e;
+    }
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+}
